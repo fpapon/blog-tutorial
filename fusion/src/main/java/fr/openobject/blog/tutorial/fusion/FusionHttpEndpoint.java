6bb5c983e2c4a52dff191804fe982fd43ad23146
francois papon <francois.papon@openobject.fr>
2023-01-13 21:44:38 +0100

    [fusion] add example

---
+++ fusion/src/main/java/fr/openobject/blog/tutorial/fusion/FusionHttpEndpoint.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019-Present - François Papon - Openobject.fr - https://openobject.fr
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package fr.openobject.blog.tutorial.fusion;
+
+import fr.openobject.blog.tutorial.fusion.model.Customer;
+import io.yupiik.fusion.framework.build.api.scanning.Bean;
+import io.yupiik.fusion.framework.build.api.scanning.Injection;
+import io.yupiik.fusion.http.server.api.Response;
+import io.yupiik.fusion.http.server.spi.Endpoint;
+import io.yupiik.fusion.json.JsonMapper;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class FusionHttpEndpoint {
+
+    @Injection
+    FusionConfiguration conf;
+
+    @Injection
+    JsonMapper jsonMapper;
+
+    @Bean
+    public Endpoint customerApi() {
+        return Endpoint.of(
+                request -> "GET".equals(request.method()) &&
+                        request.path().startsWith("/customers"),
+
+                request -> CompletableFuture.completedStage(
+                        Response.of()
+                        .status(200)
+                        .header("content-type", "application/json")
+                        .body(jsonMapper.toString(
+                                new Customer(UUID.randomUUID().toString(), "obiwan", "kenobi", "master", conf.organization())))
+                        .build()));
+    }
+}
---
+++ fusion/src/main/java/fr/openobject/blog/tutorial/fusion/FusionMain.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019-Present - François Papon - Openobject.fr - https://openobject.fr
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package fr.openobject.blog.tutorial.fusion;
+
+import io.yupiik.fusion.framework.api.ConfiguringContainer;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Logger;
+
+import static java.lang.System.exit;
+
+public class FusionMain {
+
+    private static final Logger logger = Logger.getLogger(FusionMain.class.getName());
+    public static void main(String[] args) {
+        try (final var container = ConfiguringContainer.of().start()) {
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+            logger.info("started!");
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            exit(1);
+        }
+        exit(0);
+    }
+}
